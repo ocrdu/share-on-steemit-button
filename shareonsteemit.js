83ea3e967355848f3310e25518d7d3c926d30606
ocrdu <oscar@den-uijl.nl>
2019-05-14 16:09:56 +0200

    Added a hardcoded tag in shareonsteemit.js

--- shareonsteemit.js
+++ shareonsteemit.js
@@ -29,27 +29,27 @@ function check() {
   var title = document.getElementById("title").value;
   var article = document.getElementById("article").value;
   if (title.length>0 && article.length>0 &&
-    account.length > 2  &&
-    account.length < 17  &&
+      account.length > 2  &&
+      account.length < 17  &&
 	  /^[a-z0-9\-\.]+$/.test(account)  &&
 	  /^[a-z]$/.test(account.charAt(0))  &&
 	  /^[a-z0-9]$/.test(account.charAt(account.length-1))  &&
-    key.length == 51 && 
+      key.length == 51 && 
 	  key.charAt(0) == "5" && 
 	  /^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]+$/.test(key) && 
 	  title.length > 0 && 
 	  article.length > 0) {
     document.getElementById("postbutton").disabled = false;
-    document.getElementById("postbutton").style.backgroundColor = "#06D6A9";
-    document.getElementById("postbutton").style.color = "black";
-    document.getElementById("postbutton").style.borderColor = "#5FBA7D";
-    document.getElementById("postbutton").style.cursor = "pointer";
+	document.getElementById("postbutton").style.backgroundColor = "#06D6A9";
+	document.getElementById("postbutton").style.color = "black";
+	document.getElementById("postbutton").style.borderColor = "#5FBA7D";
+	document.getElementById("postbutton").style.cursor = "pointer";
   } else {
     document.getElementById("postbutton").disabled = true;
     document.getElementById("postbutton").style.backgroundColor = "#CCCCCC";
-    document.getElementById("postbutton").style.color = "#AAAAAA";
-    document.getElementById("postbutton").style.borderColor = "";
-    document.getElementById("postbutton").style.cursor = "default";
+	document.getElementById("postbutton").style.color = "#AAAAAA";
+	document.getElementById("postbutton").style.borderColor = "";
+	document.getElementById("postbutton").style.cursor = "default";
   }
 }
 
